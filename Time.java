be4edf08dc5b9d889c2c86f0166b105a632b5b20
kaito071831 <kaito071831@gmail.com>
2021-07-07 15:27:20 +0900

    Complete start event and create stop event

--- Time.java
+++ Time.java
@@ -2,14 +2,12 @@ public class Time {
 
     int min;
     int sec;
-    int totalSec;
     String minO;
     String secO;    
 
     public Time(){
         min = 0;
         sec = 0;
-        totalSec = 0;
         minO = "0";
         secO = "0";
     }
@@ -18,17 +16,8 @@ public class Time {
         return min;
     }
 
-    public String  outputTime(int minI){
-        minO = Integer.toString(minI);
-        return minO;
-    }
-
     public int getSec(){
         return sec;
-    }
-
-    public int getTotalSec(){
-        return totalSec = min*60+sec;
     }
     
 }
